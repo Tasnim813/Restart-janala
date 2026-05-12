00db0882f1e4a2cdb902ed1f828c2fd3c4838484
Tasnim813 <tasnim3j@gmail.com>
2026-05-12 23:10:46 +0600

    daynamic btn

--- script/index.js
+++ script/index.js
@@ -6,10 +6,15 @@ const loadLesson=()=>{
 const displayLesson=(lessons)=>{
     console.log(lessons)
     const levelContainer=document.getElementById('level-container')
+    levelContainer.innerHTML= ' ';
     for(let lesson of lessons){
+        console.log(lesson)
         const btnDiv=document.createElement("div")
         btnDiv.innerHTML=`
+        <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson ${lesson.level_no
+} </button>
         `
+        levelContainer.append(btnDiv)
     }
 
 }
