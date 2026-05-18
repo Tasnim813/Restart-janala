2f885e11988d7051a9341b30b445c467fba2cef7
Tasnim813 <tasnim3j@gmail.com>
2026-05-18 18:10:38 +0600

    volume add to speker

--- script/index.js
+++ script/index.js
@@ -3,6 +3,11 @@ const createElement=(arr)=>{
     return(htmlElements.join(" "))
 
 }
+function pronounceWord(word) {
+  const utterance = new SpeechSynthesisUtterance(word);
+  utterance.lang = "en-EN"; // English
+  window.speechSynthesis.speak(utterance);
+}
 const manageSpinner=(status)=>{
   if(status== true){
     document.getElementById("spinner").classList.remove("hidden")
@@ -105,7 +110,7 @@ const displayLevelWord=(words)=>{
         </div>
         <div class="flex justify-between text-center">
           <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80] "><i class="fa-solid  fa-circle-info" ></i></button>
-          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid   fa-volume-high"></i></button>
+          <button onClick="pronounceWord( '${word.word}')" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid   fa-volume-high"></i></button>
 
         </div>
 
